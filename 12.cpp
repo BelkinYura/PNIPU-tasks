2348fda8b7fc120291da6336b6394cd9a5de3fc8
BelkinYura <93118559+BelkinYura@users.noreply.github.com>
2021-10-30 17:51:24 +0500

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int x;
+    int a;
+    int dif;
+    cin >> x;
+    cout << "Угадайте число за 7 попыток" << endl;
+    for (int i = 0; i < 7; i++) {
+        cin >>a;
+        if (a!=x) 
+        {
+            if (a>x)
+            {
+                dif = a-x;
+                cout << "Не угадали. Число меньше на "<<dif<<endl;
+            }
+            if (a<x)
+            {
+                dif = x-a;
+                cout << "Не угадали. Число больше на "<<dif<<endl;
+            }
+        }
+        else
+        {
+            cout << "Угадали"<<endl;
+            break;
+        }
+    }
+    return 0;
+}
